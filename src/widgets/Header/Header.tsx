3d0593cdedd6b5dbd6474012143729f09a3fdd64
redo_oreon <sokolovskii.vlad2011@mail.ru>
2023-09-18 18:28:16 +0300

    add links on pages in Header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,6 +1,7 @@
+import { Link } from "react-router-dom";
 import { useCart } from "shared/hooks/useCart";
 import { classNames } from "shared/lib/classNames/classNames";
-import { useItems } from "../../shared/hooks/useItems";
+import { useItems } from "shared/hooks/useItems";
 import cls from "./Header.module.scss";
 
 interface HeaderProps {
@@ -13,7 +14,7 @@ export const Header = ({ className }: HeaderProps) => {
 
   return (
     <header className={classNames(cls.Header, {}, [className])}>
-      <div className={cls.left}>
+      <Link className={cls.left} to={"/"}>
         <div className="logo">
           <img width={40} height={40} src="/icons/logo.svg" alt="Логотип" />
         </div>
@@ -21,23 +22,38 @@ export const Header = ({ className }: HeaderProps) => {
           <h3 className={cls.text}>React Sneakers</h3>
           <div className={cls.desc}>Магазин лучших кроссовок</div>
         </div>
-      </div>
+      </Link>
       <ul className={cls.right}>
         <li className={cls.cart}>
           <button onClick={showCart}>
-            <img width={18} height={18} src="/icons/cart.svg" alt="" />
+            <img
+              width={18}
+              height={18}
+              src="/icons/cart.svg"
+              alt="Иконка корзины"
+            />
             <div className={cls.cartSum}>{getItemsInCartTotal()} руб.</div>
           </button>
         </li>
         <li>
-          <button>
-            <img width={18} height={18} src="/icons/favorites.svg" alt="" />
-          </button>
+          <Link to={"/favorites"}>
+            <img
+              width={18}
+              height={18}
+              src="/icons/favorites.svg"
+              alt="Иконка избранного"
+            />
+          </Link>
         </li>
         <li>
-          <button>
-            <img width={18} height={18} src="/icons/user.svg" alt="" />
-          </button>
+          <Link to={"/profile"}>
+            <img
+              width={18}
+              height={18}
+              src="/icons/user.svg"
+              alt="Иконка профиля"
+            />
+          </Link>
         </li>
       </ul>
     </header>
